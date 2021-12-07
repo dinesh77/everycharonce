0fb277e1620299e7ba46a9acc8ea37cd0346ef6e
dinesh77 <dinesh.nandhi@gmail.com>
2021-12-07 06:17:48 -0500

    Add files via upload

---
+++ String_Pro8.java
@@ -0,0 +1,31 @@
+//Req: Write a program to print every character only once
+
+package com.javalaya.strings.practice;
+
+class String_Pro8{
+	
+	public static void m(String s){
+		String res = "";
+		for(int i=0;i<s.length();i++){
+			if(!res.contains(String.valueOf(s.charAt(i)))){
+				res = res.concat(String.valueOf(s.charAt(i)));
+			}
+		}
+		System.out.println(res);
+	}
+}
+
+class String_Pro8_Test{
+	
+	public static void main(String...abc){
+		System.out.println("\n\n/*************** OUTPUT ***************\n");
+		String_Pro8.m("Javalaya students");
+		System.out.println("\n*************** EXECUTION COMPLETED *****************/");
+	}
+}
+
+/*************** OUTPUT ***************
+
+Javly studen
+
+*************** EXECUTION COMPLETED *****************/
